120ab980e20b19d318ecd6d1d94c241648c17480
lcnde <176673026+lcnde@users.noreply.github.com>
2021-01-19 20:54:36 +0100

    Temporarily commented the before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [:new, :create]
+  #before_action :require_login, only: [:new, :create]
 
   def new
     @post = Post.new
@@ -13,12 +13,13 @@ class PostsController < ApplicationController
       render :new
     end
   end
-
+=begin
   private
   def require_login
     unless logged_in?
       flash[:error] = "You must be logged in to create a post"
       redirect_to new_login_url #to be added later
     end
-  end
+  end 
+=end
 end
