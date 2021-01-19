9d49337e12552c1ac55b691b6f4e7062c43854f2
lcnde <176673026+lcnde@users.noreply.github.com>
2021-01-19 20:23:29 +0100

    Set up the before_action with its private method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
+  def 
+
+  private
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to create a post"
+      redirect_to new_login_url #to be added later
+    end
+  end
 end
