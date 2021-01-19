dbdfd85c2e1dd934bd91ced203883a7f347489b2
lcnde <176673026+lcnde@users.noreply.github.com>
2021-01-19 20:15:34 +0100

    ran migration for post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_185145) do
+ActiveRecord::Schema.define(version: 2021_01_19_185817) do
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "username"
